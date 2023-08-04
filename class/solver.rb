efccf1db525645e87c88dcc55afe17703ca61bd6
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-04 10:47:53 +0200

    Create method fizzbuzz and test result correct" git push origin tdd-approach
    c
    
    "

--- class/solver.rb
+++ class/solver.rb
@@ -6,4 +6,11 @@ class Solver
     
         number * factorial(number - 1)
     end
+    def fizzbuzz(number)
+        return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+        return 'fizz' if (number % 3).zero?
+        return 'buzz' if (number % 5).zero?
+    
+        number.to_s
+      end
 end
