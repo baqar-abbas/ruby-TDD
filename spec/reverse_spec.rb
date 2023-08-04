a9e44dbcbfebad2b166651e1b1ede359639ce557
baqar-abbas <baqar007@hotmail.com>
2023-08-04 13:49:40 +0500

    Revere string test case defined using TDD approach and now pushing test file. Next we will defined the method

---
+++ spec/reverse_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../class/solver'
+require 'rspec'
+require 'rspec/mocks'
+
+describe Solver do
+
+    let(:solver) { Solver.new }
+
+    describe '#reverse_string' do
+    
+        it 'takes string as a parameter and reverses it' do
+            allow(solver).to receive(:reverse_string).with('Hello').and_return('olleH')
+      
+            expect(solver.reverse_string('Hello')).to eq('olleH')
+          end
+
+    end
+
+end
+
