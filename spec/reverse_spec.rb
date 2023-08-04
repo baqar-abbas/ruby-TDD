e66a230e8154d0f49c564cafab78263631109434
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-04 10:54:12 +0200

    Fix rubocop

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -3,18 +3,13 @@ require 'rspec'
 require 'rspec/mocks'
 
 describe Solver do
+  let(:solver) { Solver.new }
 
-    let(:solver) { Solver.new }
+  describe '#reverse_string' do
+    it 'takes string as a parameter and reverses it' do
+      allow(solver).to receive(:reverse_string).with('Hello').and_return('olleH')
 
-    describe '#reverse_string' do
-    
-        it 'takes string as a parameter and reverses it' do
-            allow(solver).to receive(:reverse_string).with('Hello').and_return('olleH')
-      
-            expect(solver.reverse_string('Hello')).to eq('olleH')
-          end
-
+      expect(solver.reverse_string('Hello')).to eq('olleH')
     end
-
+  end
 end
-
