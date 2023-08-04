f7d41a99afc611c81cd1a83fb06fe4132cd757ba
baqar-abbas <baqar007@hotmail.com>
2023-08-04 16:03:20 +0500

    Suggested changes by the code reviewer has been incorporated.

--- class/solver.rb
+++ class/solver.rb
@@ -1,7 +1,7 @@
 class Solver
   def factorial(number)
     raise ArgumentError, 'Number must be non-negative' if number.negative?
-    return 0 if number.zero?
+    return 1 if number.zero?
     return 1 if number == 1
 
     number * factorial(number - 1)
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -6,10 +6,10 @@ describe Solver do
   let(:solver) { Solver.new }
 
   describe '#factorial' do
-    it 'returns zero if the parameter is zero' do
-      allow(solver).to receive(:factorial).with(0).and_return(0)
+    it 'returns one if the parameter is zero' do
+      allow(solver).to receive(:factorial).with(0).and_return(1)
 
-      expect(solver.factorial(0)).to eq(0)
+      expect(solver.factorial(0)).to eq(1)
     end
 
     it 'returns 1 if the parameter is 1' do
